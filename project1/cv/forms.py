097616fc8f5e87cb4ff91f52bc5efb312bc076ec
Shanmugaraj Kamaraj <33391433+vishakraj25@users.noreply.github.com>
2021-08-31 07:48:31 +0000

    Update forms.py

--- project1/cv/forms.py
+++ project1/cv/forms.py
@@ -1,19 +1,5 @@
 from django import forms
 import os
-'''
-class ClassForm(forms.Form):
-	with open(os.path.join(os.getcwd(), "cv", "coco_names.txt"), 'r') as f:
-		classes = [w.strip() for w in f.readlines()]
-	classes1 = []
-	for i in range(len(classes)):
-		classes1.append((str(classes[i]), str(classes[i])))
-	print('classes in forms', classes1, [c[0] for c in classes1])
-	video_location = forms.CharField()
-	classes_field = forms.MultipleChoiceField(choices = classes1,
-												#widget=forms.CheckboxSelectMultiple, 
-												initial=['person', 'laptop'],#[c[0] for c in classes1],
-												)
-'''
 	
 class DeleteForm(forms.Form):
 	delete_field = forms.IntegerField(help_text = "Enter number days")
@@ -25,14 +11,16 @@ class ClassForm(forms.Form):
 	classes1 = []
 	for i in range(len(classes)):
 		classes1.append((str(classes[i]), str(classes[i])))
-	#print('classes in forms', classes1)
 	video_location = forms.CharField()
 	classes_field = forms.MultipleChoiceField(choices = classes1,
-												#initial=[c[0] for c in classes1],
-												)
+		#initial=[c[0] for c in classes1],
+		)
+	
 	def __init__(self, request, *args, **kwargs):
-		#session_icons = kwargs.pop('session_icons')
-		print('session_icons', request.session['classes_field'])
-		super(ClassForm, self).__init__(*args, **kwargs)
-		self.fields['classes_field'].initial = request.session['classes_field']
-                
+		try:
+		    print('session', request.session['classes_field'])
+		    super(ClassForm, self).__init__(*args, **kwargs)
+		    self.fields['classes_field'].initial = request.session['classes_field']
+		except Exception as e:
+		    super(ClassForm, self).__init__(*args, **kwargs)
+		    self.fields['classes_field'].initial = ['person']
